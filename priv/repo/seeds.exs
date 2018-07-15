23f8380c5d2ce9d200a31fa3c01c711757c953de
Ivan Gonzalez <hi@dreamingecho.es>
2018-07-16 00:00:48 +0200

    Add example user to seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,9 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Bloggex.Repo.insert!(%Bloggex.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Bloggex.Accounts
+
+# Create a default user
+Accounts.create_user(%{
+  email: "admin@example.com",
+  password: "123456",
+  name: "Example",
+  surname: "Example"
+})
