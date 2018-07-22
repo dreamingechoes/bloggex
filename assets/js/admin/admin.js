2da8b921ef1f7772f1c9d135a245d6777eae87ce
Ivan Gonzalez <hi@dreamingecho.es>
2018-07-22 22:50:03 +0200

    Add datable files to admin css and js

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -3,5 +3,9 @@ window.$ = window.jquery = jQuery;
 import "phoenix_html";
 
 // Vendor scripts
+import "datatables.net/js/jquery.dataTables";
+import "datatables.net-bs/js/dataTables.bootstrap";
 import "bootstrap/dist/js/bootstrap";
 import "admin-lte/dist/js/adminlte";
+
+$('.table').dataTable();
