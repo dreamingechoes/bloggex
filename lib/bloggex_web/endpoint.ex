d1f4f7563598c57a6cfb8a2ab7d0f465a252b2bf
Ivan Gonzalez <hi@dreamingecho.es>
2018-07-25 20:56:59 +0200

    Add new plug static for uploaded files

--- lib/bloggex_web/endpoint.ex
+++ lib/bloggex_web/endpoint.ex
@@ -12,7 +12,15 @@ defmodule BloggexWeb.Endpoint do
     at: "/",
     from: :bloggex,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js uploads favicon.ico robots.txt)
+  )
+
+  plug(
+    Plug.Static,
+    at: "/priv/static",
+    from: :bloggex,
+    gzip: false,
+    only: ~w(uploads)
   )
 
   # Code reloading can be explicitly enabled under the
